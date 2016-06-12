0554bcb40da5caeaf15922fd35f2a197b8380fee
Hikolo <Hikolo.92@gmail.com>
2016-06-12 15:04:32 +0200

    Add files via upload

---
+++ sketch_jun11a.ino
@@ -0,0 +1,35 @@
+/*
+/* Grove - Temperature Sensor demo v1.0
+*  This sensor detects the environment temperature,
+*  Connect the signal of this sensor to A0, use the 
+*  Serial monitor to get the result.
+*  By: http://www.seeedstudio.com
+*/
+#include <math.h>
+int a;
+String s;
+float temperature;
+int B=3975;                  //B value of the thermistor
+float resistance;
+int getTemp(int pin){
+  a=analogRead(pin);
+  resistance=(float)(1023-a)*10000/a; //get the resistance of the sensor;
+  temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;//convert to temperature via datasheet&nbsp;;
+  return temperature;
+}
+String readSerial(){
+    if (Serial.available() > 0) { //Check if there's data in the serial port
+    s = Serial.readString();
+    return s;
+}
+void setup()
+{
+  Serial.begin(9600);  
+}
+ 
+void loop()
+{
+  Serial.print("Current temperature is ");
+  Serial.println(temperature);
+  delay(1000);
+ }
