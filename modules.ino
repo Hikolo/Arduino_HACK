5c029851abeb580466cd73009b809d5b493d3814
Karl Stenlund <Karl Stenlund>
2016-07-10 15:06:14 +0200

    Added to loop

--- modules.ino
+++ modules.ino
@@ -61,7 +61,7 @@ class Relay
   }
   boolean Off()
   {
-    if (State() == false)
+    if (!State())
     {
       return false;
     }
@@ -77,6 +77,78 @@ class Relay
 
   
 };
-void setup(){}
-void loop(){}
+class Light
+{
+  int lightPin;
+  int threshold;
+  int sensorValue;
+  float resistance;
+  
+  public:
+  Light(int pin, int th = 10)
+  {
+    lightPin = pin;
+    threshold = th;
+  }
+  boolean check()
+  {
+    int sensorValue = analogRead(lightPin); 
+    resistance = (float)(1023-sensorValue)*10/sensorValue;
+    if (resistance > threshold)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  int value()
+  {
+    sensorValue = analogRead(lightPin); 
+    resistance = (float)(1023-sensorValue)*10/sensorValue;
+    return resistance;
+  }
+  
+};
+
+  
+  Therm T1(0);
+  Relay R1(2);
+  Light L1(1,10);
+
+
+
+void setup()
+{
+
+  
+}
+
+
+void loop()
+{
+  if( Serial.available() > 0)
+  {
+    String r = Serial.readString();
+    if (r.charAt(0) == 'R') {
+      if ( r.charAt(2) == 'O' ) {
+        R1.On();
+      }
+      else {
+        R1.Off();
+      }
+      R1.Update();
+    }
+    else if (r.charAt(0) == 'T') {
+      Serial.print(T1.getTemp());
+    }
+    else {
+      Serial.print(L1.value());
+    }
+      
+        
+    
+  }
+}
 
